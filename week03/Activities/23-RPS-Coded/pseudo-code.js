e3b9f01d6fc530a260277dda541cd43051f7c085
Kyle <k90armstrong@gmail.com>
2017-09-29 17:13:28 -0600

    changes toeverything

---
+++ week03/Activities/23-RPS-Coded/pseudo-code.js
@@ -0,0 +1,8 @@
+// user selects, rock, paper or scissors
+
+// the computer would randomly pick one of the options
+// check to see who won
+// if the computer won, increment it;s wins
+// if the user won increment the user's wins 
+// if no one wins, do nothing
+// play again
